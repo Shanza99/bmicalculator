e4d71402fceccee167866ad49c12ad6b1471b6cc
shanza99 <shanzabatool001@gmail.com>
2024-11-05 10:09:29 +0500

    video 08(make container seperate file)
    make container seperate file

--- bmi/lib/container.dart
+++ bmi/lib/container.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'IconText.dart';
-import 'ContainerFile.dart';
 class RepeatRefactorCode extends StatelessWidget {
   final Color colors;
   final Widget? cardwidget;
